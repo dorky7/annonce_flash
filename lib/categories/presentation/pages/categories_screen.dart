46aa4421858e53d3d51a6722afd600a0363a4b42
Developer <Developer@hintekks-MacBook-Pro-2.local>
2024-11-18 09:51:13 +0100

    categorie and profil + register

--- lib/categories/presentation/pages/categories_screen.dart
+++ lib/categories/presentation/pages/categories_screen.dart
@@ -12,33 +12,72 @@ class CategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("categories"),
+        title: const Text("Catégories"),
+        backgroundColor: Colors.teal,
       ),
       body: BlocBuilder<CategoryListBloc, CategoryListState>(
         builder: (context, state) {
-
-
-     if (state is CategoryListState) {
-        getIt.get<CategoryListBloc>().add(FetchCategoryListEvent());
-     }else
-    if (state is FetchCategoryListLoading) {
-      return const Center(
-        child: CircularProgressIndicator(
-          color:  Colors.amber,
-        ),
-      );
-    }
-    
-    
-    if (state is FetchCategoryListSuccess) {
-      return ListView.builder(itemBuilder: (context , index){
-        return  ListTile(
-          title: Text(state.categories![index].name),
-        );
-      });
-    }
-      return Container();
-    },
+          if (state is CategoryListInitial) {
+            getIt.get<CategoryListBloc>().add(FetchCategoryListEvent());
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+              ),
+            );
+          } else if (state is FetchCategoryListLoading) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Colors.amber,
+              ),
+            );
+          } else if (state is FetchCategoryListSuccess) {
+            return ListView.builder(
+              itemCount: state.categories?.length,
+              itemBuilder: (context, index) {
+                final category = state.categories?[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: const Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    child: ListTile(
+                      leading: const Icon(
+                        Icons.category,
+                        color: Colors.teal,
+                      ),
+                      title: Text(
+                        category!.name,
+                        style: const TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      trailing: const Icon(
+                        Icons.arrow_forward_ios,
+                        color: Colors.teal,
+                      ),
+                      onTap: () {
+                      },
+                    ),
+                  ),
+                );
+              },
+            );
+          } else if (state is FetchCategoryListFaillure) {
+            return Center(child: Text(state.message!));
+          }
+          return Container();
+        },
       ),
     );
   }
